c1817b9416d75dac94e6082b810eee252fe03ce4
Wilky0 <101wilky@gmail.com>
2017-09-05 00:35:15 +0100

    Added basic base prefab
    Adds a base object, the script currently starts with a health value that decreases with every mob that touches it,  the mob is destroyed on collision. Once the base runs out of health it is destryed. Need to add a gameover call at that point!

---
+++ Assets/Scripts/PlayerBaseScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBaseScript : MonoBehaviour {
+    public int health = 50;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0)
+        {
+            BaseDestroyed();
+        }
+	}
+
+    void BaseDestroyed ()
+    {
+        // Insert game over call here!!!
+        Destroy(this.gameObject);
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Mob(Clone)")
+        {
+            Destroy(col.gameObject);
+            health--;
+        }
+    }
+}
