886390fa23a4fd220a7596af30be6536e5a6542c
Wilky0 <101wilky@gmail.com>
2017-09-07 00:54:43 +0100

    Calculate screen bounds
    Added a calculation to the LevelScrip to allow other scripts to get the bounds. THis will be used by the path generator to make sure the patch is witing the screen

--- Assets/Scripts/LevelScript.cs
+++ Assets/Scripts/LevelScript.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class LevelScript : MonoBehaviour {
     private bool GameOver = false;
-
+    private float screenLeft;
+    private float screenRight;
 	// Use this for initialization
 	void Start () {
-		
-	}
+        Vector3 p = Camera.main.ScreenToWorldPoint(new Vector3(0, 0, Camera.main.nearClipPlane));
+        screenLeft = p.x;
+        p = Camera.main.ScreenToWorldPoint(new Vector3(Camera.main.pixelWidth, 0, Camera.main.nearClipPlane));
+        screenRight = p.x;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //Debug.Log("Left : " + screenLeft + " Right : " + screenRight);
 	}
 
     public void SetGameOver(bool over)
     {
         GameOver = over;
         Debug.Log("GameOver");
+    }
+
+    public float GetLeftScreenEdge()
+    {
+        return screenLeft;
+    }
+
+    public float GetRightScreenEdge()
+    {
+        return screenRight;
     }
 }
