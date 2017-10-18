480981f1fe81bbf156f31211eb33055116084eac
Wilky0 <101wilky@gmail.com>
2017-10-19 00:09:19 +0100

    New script for base towers
    neede to push before pc wipe

---
+++ Assets/Scripts/TowerBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerBase : MonoBehaviour {
+
+    public float ReloadTime;
+    public float Range;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
